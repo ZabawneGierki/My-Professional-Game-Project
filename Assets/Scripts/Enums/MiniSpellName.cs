8cb3a11187f35c9b696e75fe8f5924ac9b5ac56d
krzysztofskorek <57440106+krzysztofskorek@users.noreply.github.com>
2026-07-16 18:46:27 +0200

    added spell name enum

---
+++ Assets/Scripts/Enums/MiniSpellName.cs
@@ -0,0 +1,13 @@
+
+
+/// <summary>
+/// This enum represents the items that can be available through quick access in the game, such as potions. Each item is represented by a unique name.
+/// </summary>
+public enum MiniSpellName
+{
+    HealingPotion,
+    ManaPotion,
+    Freefall 
+
+}
+ 
